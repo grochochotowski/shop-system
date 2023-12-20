b94771bf9a9fcea85fde43a33febfaab8598823e
grochochotowski <g.m.rochowski@gmail.com>
2023-12-20 14:22:56 +0100

    Create clothing availibility entity ! needs to be added to other entities

---
+++ shop-system/shop-system/Entities/ClothingAvailability.cs
@@ -0,0 +1,13 @@
+﻿namespace shop_system.Entities
+{
+    public class ClothingAvailability
+    {
+        public int Id { get; set; }
+        public int ClothingId { get; set; }
+        public Clothing Clothing { get; set; }
+        public int Quantity { get; set; }
+
+        public int ShopId { get; set; }
+        public Shop Shop { get; set; }
+    }
+}
