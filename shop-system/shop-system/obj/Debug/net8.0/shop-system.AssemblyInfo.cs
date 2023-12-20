5c9fc73544c99c9238897a80e7243861201f921d
grochochotowski <g.m.rochowski@gmail.com>
2023-12-20 13:17:39 +0100

    Delete example api files

--- shop-system/shop-system/obj/Debug/net8.0/shop-system.AssemblyInfo.cs
+++ shop-system/shop-system/obj/Debug/net8.0/shop-system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("shop-system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d118083655cfa861e3cf4544f7e35cfb3757e02")]
 [assembly: System.Reflection.AssemblyProductAttribute("shop-system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("shop-system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
