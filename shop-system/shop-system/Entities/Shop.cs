b25ee354df3106d8eb2b8ec8dc22a976e829b60a
grochochotowski <g.m.rochowski@gmail.com>
2023-12-20 13:30:27 +0100

    Create shop entity

---
+++ shop-system/shop-system/Entities/Shop.cs
@@ -0,0 +1,19 @@
+﻿namespace shop_system.Entities
+{
+    public class Shop
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string Region { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Building { get; set; }
+        public string Local { get; set; }
+        public string PostalCode { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public int ClothingId { get; set; }
+        public virtual List<Clothing> Clothes { get; set; }
+    }
+}
