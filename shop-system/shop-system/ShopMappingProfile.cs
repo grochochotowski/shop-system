a1b6f59b8ee60c2770ddaffa3fdc50a144dfcfc6
grochochotowski <g.m.rochowski@gmail.com>
2023-12-22 23:16:51 +0100

    Create mapping profile for shop

---
+++ shop-system/shop-system/ShopMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using shop_system.Entities;
+using shop_system.Models;
+using System.Runtime.InteropServices;
+
+namespace shop_system
+{
+    public class ShopMappingProfile : Profile
+    {
+        public ShopMappingProfile()
+        {
+            CreateMap<Shop, ShopDto>();
+            CreateMap<ClothingAvailability, ClothingAvailabilityDto>();
+            CreateMap<Clothing, ClothingDto>();
+        }
+    }
+}
