02ef740a698bf38fdae3ff24144442e9e475ca18
grochochotowski <g.m.rochowski@gmail.com>
2023-12-22 22:59:02 +0100

    Create get all shops controller

---
+++ shop-system/shop-system/Controllers/ShopController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using shop_system.Entities;
+
+namespace shop_system.Controllers
+{
+    [Route("api/shop")]
+    public class ShopController : ControllerBase
+    {
+        private readonly ShopDbContext _context;
+        public ShopController(ShopDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Shop>> GetAll()
+        {
+            var shops = _context
+                .Shops
+                .ToList();
+            return Ok(shops);
+        }
+    }
+}
--- shop-system/shop-system/obj/Debug/net8.0/shop-system.AssemblyInfo.cs
+++ shop-system/shop-system/obj/Debug/net8.0/shop-system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("shop-system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4f45b4804a7dc8e02960903a34597ca948f47e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("shop-system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("shop-system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
