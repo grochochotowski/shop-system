9757652cff15167d6382b32097c6331a3297c136
grochochotowski <g.m.rochowski@gmail.com>
2023-12-22 23:17:55 +0100

    Create Dto models for existing tables

---
+++ shop-system/shop-system/Models/ClothingAvailabilityDto.cs
@@ -0,0 +1,13 @@
+﻿using shop_system.Entities;
+
+namespace shop_system.Models
+{
+    public class ClothingAvailabilityDto
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public int ClothingId { get; set; }
+
+        public ClothingDto Clothing { get; set; }
+    }
+}
---
+++ shop-system/shop-system/Models/ClothingDto.cs
@@ -0,0 +1,11 @@
+﻿namespace shop_system.Models
+{
+    public class ClothingDto
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public string Colour { get; set; }
+        public int Size { get; set; }
+        public double Price { get; set; }
+    }
+}
