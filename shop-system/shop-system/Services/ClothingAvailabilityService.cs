b1d53655af6297e01036132312f74f0d3e8b1789
grochochotowski <g.m.rochowski@gmail.com>
2023-12-26 00:19:23 +0100

    Create service for clothing availability with Create() method

---
+++ shop-system/shop-system/Services/ClothingAvailabilityService.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using shop_system.Entities;
+using shop_system.Models;
+
+namespace shop_system.Services
+{
+    public interface IClothingAvailabilityService
+    {
+        int Create(int shopId, ClothingAvailabilityDto dto);
+    }
+
+    public class ClothingAvailabilityService : IClothingAvailabilityService
+    {
+        private readonly ShopDbContext _context;
+        private readonly IMapper _mapper;
+
+        public ClothingAvailabilityService(ShopDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+
+        public int Create(int shopId, ClothingAvailabilityDto dto)
+        {
+            var shop = _context
+                .Shops
+                .FirstOrDefault(s => s.Id == shopId);
+            if (shop is null) return -1;
+
+            var availabilityEntity = _mapper.Map<ClothingAvailability>(dto);
+            _context.ClothingAvailability.Add(availabilityEntity);
+            _context.SaveChanges();
+            return availabilityEntity.Id;
+        }
+    }
+}
