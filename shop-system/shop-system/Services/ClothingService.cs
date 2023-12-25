e248320f75a51545d64519ecae79ad45bb40122d
grochochotowski <g.m.rochowski@gmail.com>
2023-12-25 22:26:47 +0100

    Create base clothing service structure

---
+++ shop-system/shop-system/Services/ClothingService.cs
@@ -0,0 +1,32 @@
+﻿using shop_system.Models;
+
+namespace shop_system.Services
+{
+    public class ClothingService
+    {
+        public IEnumerable<ClothingDto> GetAll()
+        {
+
+            return result;
+        } // Get all clothes
+
+        public ClothingDto Get(int id)
+        {
+
+            return result;
+        } // Get clothing by ID
+
+        public int Add(ClothingDto dto)
+        {
+
+            return result;
+        } // Add new clothing
+
+        public bool Delete(int id)
+        {
+
+
+            return true;
+        } // Delete clothing by ID
+    }
+}
