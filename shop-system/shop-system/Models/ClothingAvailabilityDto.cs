1ae3b3ce951c5352a08d5053b551d34e7d2970f0
grochochotowski <g.m.rochowski@gmail.com>
2023-12-26 00:12:00 +0100

    Update dtos for clothing avaialbility

--- shop-system/shop-system/Models/ClothingAvailabilityDto.cs
+++ shop-system/shop-system/Models/ClothingAvailabilityDto.cs
@@ -4,7 +4,10 @@ namespace shop_system.Models
 {
     public class ClothingAvailabilityDto
     {
+        public int Id { get; set; }
         public int Quantity { get; set; }
         public int ClothingId { get; set; }
+
+        public ClothingDto Clothing { get; set; }
     }
 }
---
+++ shop-system/shop-system/Models/CreateClothingAvailabilityDto.cs
@@ -0,0 +1,8 @@
+﻿namespace shop_system.Models
+{
+    public class CreateClothingAvailabilityDto
+    {
+        public int Quantity { get; set; }
+        public int ClothingId { get; set; }
+    }
+}
