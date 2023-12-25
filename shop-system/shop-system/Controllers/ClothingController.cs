bfffd98164029844c93f17f986149f3b559e97d3
grochochotowski <g.m.rochowski@gmail.com>
2023-12-25 22:34:59 +0100

    Create base clothing controller structure

---
+++ shop-system/shop-system/Controllers/ClothingController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using shop_system.Models;
+using shop_system.Services;
+
+namespace shop_system.Controllers
+{
+    [Route("api/clothing")]
+    public class ClothingController
+    {
+        private readonly IClothingService _clothingService;
+
+        public ClothingController(IClothingService clothingService)
+        {
+            _clothingService = clothingService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ClothingDto>> GetAll()
+        {
+
+            return Ok();
+        } // Get all clothes
+
+        [HttpGet("{id}")]
+        public ActionResult<ShopDto> Get([FromRoute] int id)
+        {
+
+            return Ok();
+        } // Get clothing by ID
+
+        [HttpPost]
+        public ActionResult AddClothing([FromBody] ClothingDto dto)
+        {
+
+            return Created();
+        } // Add new clothing
+
+        [HttpDelete("{id}")]
+        public ActionResult RemoveClothing([FromRoute] int id)
+        {
+
+
+            return NoContent();
+        } // Delete clothing by ID
+    }
+}
