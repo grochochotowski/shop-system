d04b860be973c4646b9fa8d17a3f9040b6bd5756
grochochotowski <g.m.rochowski@gmail.com>
2023-12-25 23:01:09 +0100

    Create add clothing dto model

---
+++ shop-system/shop-system/Models/AddClothingDto.cs
@@ -0,0 +1,11 @@
+﻿namespace shop_system.Models
+{
+    public class AddClothingDto
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public string Colour { get; set; }
+        public int Size { get; set; }
+        public double Price { get; set; }
+    }
+}
