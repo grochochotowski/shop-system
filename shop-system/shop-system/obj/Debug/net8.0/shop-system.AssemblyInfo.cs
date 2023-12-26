7d41f7f20a6a85f27e4856f2208fdfa58fec9301
grochochotowski <g.m.rochowski@gmail.com>
2023-12-26 01:33:51 +0100

    Auto changes

--- shop-system/shop-system/obj/Debug/net8.0/shop-system.AssemblyInfo.cs
+++ shop-system/shop-system/obj/Debug/net8.0/shop-system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("shop-system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4db1a21e35cf3695816723d3f0abf28aea208fea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cdbd5969584469cfb538ba2290f0c58af2686fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("shop-system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("shop-system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
