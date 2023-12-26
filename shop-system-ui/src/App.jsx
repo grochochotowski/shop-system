932be8f506191fc3cce26ca1519d16f03b1272d5
grochochotowski <g.m.rochowski@gmail.com>
2023-12-26 23:30:48 +0100

    Create base of App function

---
+++ shop-system-ui/src/App.jsx
@@ -0,0 +1,15 @@
+import Button from ".components/Button";
+
+function App() {
+    return(
+        <main>
+            <h1>Welcome to the future of shop user interface</h1>
+            <h3>Choose what you want to do:</h3>
+            <div className="buttons">
+                <Button />
+            </div>
+        </main>
+    );
+}
+
+export default App;
