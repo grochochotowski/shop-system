92a99340136ebc9dc8f5c05b0bfd0c4f6ffa04b1
grochochotowski <g.m.rochowski@gmail.com>
2024-01-01 21:50:10 +0100

    Add content to header component

--- shop-system-ui/src/components/Header.jsx
+++ shop-system-ui/src/components/Header.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 
-function Header() {
+function Header(props) {
 
     return (
-        <>
-            
-        </>
+        <header>
+            <ul>
+                <li className={props.selected? "selected" : ""}>Option 1</li>
+                <li className={props.selected? "selected" : ""}>Option 2</li>
+                <li className={props.selected? "selected" : ""}>Option 3</li>
+                <li className={props.selected? "selected" : ""}>Option 4</li>
+                <li className={props.selected? "selected" : ""}>Option 5</li>
+                <li className={props.selected? "selected" : ""}>Option 6</li>
+                <li className={props.selected? "selected" : ""}>Option 7</li>
+                <li className={props.selected? "selected" : ""}>Option 8</li>
+                <li className={props.selected? "selected" : ""}>Option 9</li>
+                <li className={props.selected? "selected" : ""}>CreateUser</li>
+            </ul>
+        </header>
     );
 
 }
