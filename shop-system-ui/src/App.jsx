606b74f201c44cc810fd8b0d9345cb63293b5e76
grochochotowski <g.m.rochowski@gmail.com>
2024-01-01 21:50:52 +0100

    Display create user page

--- shop-system-ui/src/App.jsx
+++ shop-system-ui/src/App.jsx
@@ -1,10 +1,11 @@
 import Welcome from "./pages/Welcome";
 import Login from "./pages/Login";
+import CreateUser from "./pages/CreateUser";
 
 function App() {
     return(
         <>
-            <Login />
+            <CreateUser />
         </>
     );
 }
