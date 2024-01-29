cad33c7286d4627ac6b75c7523afe2381318488b
grochochotowski <g.m.rochowski@gmail.com>
2024-01-29 18:52:37 +0100

    Change window to display

--- shop-system-ui/src/App.jsx
+++ shop-system-ui/src/App.jsx
@@ -1,12 +1,13 @@
 import Welcome from "./pages/Welcome";
+import Header from "./components/Header";
 import Login from "./pages/Login";
 import CreateUser from "./pages/CreateUser";
+
 
 function App() {
     return(
         <>
-            <Welcome />
-            <Login />
+            <CreateUser />
         </>
     );
 }
