11112b365ce1b3f10362ca7aca790d3bd1e70eca
grochochotowski <g.m.rochowski@gmail.com>
2024-02-02 00:49:37 +0100

    Add titles for form sections

--- shop-system-ui/src/pages/CreateUser.jsx
+++ shop-system-ui/src/pages/CreateUser.jsx
@@ -82,6 +82,7 @@ function CreateUser() {
             <div className="create-user-container" style={{ height: containerHeight }}>
                 <section className="create-user-form" style={{ width: formWidth }}>
                     <div className="personal-section">
+                        <h3>Personal information</h3>
                         <input type="text" className="add-1st-name" placeholder="First Name" />
                         <input type="text" className="add-2nd-name" placeholder="Second Name" />
                         <input type="text" className="add-last-name" placeholder="Last Name"/>
@@ -89,12 +90,14 @@ function CreateUser() {
                     </div>
 
                     <div className="info-section">
+                        <h3>Contract information</h3>
                         <input type="email" className="add-email" placeholder="E-mail"/>
                         <input type="text" className="add-phone-number" placeholder="Phone Number"/>
                         <input type="text" className="add-account-number" placeholder="Account Number"/>
                     </div>
 
                     <div className="shop-section">
+                        <h3>Shop information</h3>
                         <input type="text" className="add-login" placeholder="Login"/>
                         <input type="password" className="add-password" placeholder="Password"/>
 
