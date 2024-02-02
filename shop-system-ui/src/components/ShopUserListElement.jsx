6e70a7c22803d0bfba3324f3fafb12c5d3e5f6b8
grochochotowski <g.m.rochowski@gmail.com>
2024-02-02 19:30:00 +0100

    Create isCollapsed to change when list is collapsed

--- shop-system-ui/src/components/ShopUserListElement.jsx
+++ shop-system-ui/src/components/ShopUserListElement.jsx
@@ -1,22 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 
 function ShopUserListElement(props) {
 
+    const [isCollapsed, setIsCollapsed] = useState(props.isCollapsed);
+
+    function GenerateList() {
+        if (!isCollapsed) {
+            return (
+                <ul>
+                    <li>User 1</li>
+                    <li>User 2</li>
+                    <li>User 3</li>
+                    <li>User 4</li>
+                    <li>User 5</li>
+                    <li>User 6</li>
+                    <li>User 7</li>
+                    <li>User 8</li>
+                    <li>User 9</li>
+                    <li>User 10</li>
+                </ul>
+            )
+        }
+    }
     return (
         <div className="shop-user-element">
             <p>Shop id number</p>
-            <ul>
-                <li>User 1</li>
-                <li>User 2</li>
-                <li>User 3</li>
-                <li>User 4</li>
-                <li>User 5</li>
-                <li>User 6</li>
-                <li>User 7</li>
-                <li>User 8</li>
-                <li>User 9</li>
-                <li>User 10</li>
-            </ul>
+            <GenerateList />
         </div>
     );
 
