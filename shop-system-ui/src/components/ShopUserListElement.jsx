f30c59b441d757c32380195e9c81152fbdbcc009
grochochotowski <g.m.rochowski@gmail.com>
2024-02-02 19:12:31 +0100

    Create element of list that appears on create user page

---
+++ shop-system-ui/src/components/ShopUserListElement.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function ShopUserListElement(props) {
+
+    return (
+        <div className="shop-user-element">
+            <p>Shop id number</p>
+            <ul>
+                <li>User 1</li>
+                <li>User 2</li>
+                <li>User 3</li>
+                <li>User 4</li>
+                <li>User 5</li>
+                <li>User 6</li>
+                <li>User 7</li>
+                <li>User 8</li>
+                <li>User 9</li>
+                <li>User 10</li>
+            </ul>
+        </div>
+    );
+
+}
+
+export default ShopUserListElement;
