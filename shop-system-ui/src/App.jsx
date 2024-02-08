fdbbd7de0e8863b17378b90dc505fd9816dd127e
grochochotowski <g.m.rochowski@gmail.com>
2024-02-08 19:32:41 +0100

    Create setup for programming of shops pages for ceo

--- shop-system-ui/src/App.jsx
+++ shop-system-ui/src/App.jsx
@@ -1,14 +1,17 @@
-import Welcome from "./pages/Welcome";
+// COMPONENTS
 import HeaderCeo from "./components/HeaderCeo";
+// PAGES
+import Welcome from "./pages/Welcome";
 import Login from "./pages/Login";
 import UserCeo from "./pages/UserCeo";
+import ShopsCeo from "./pages/ShopsCeo";
 
 
 function App() {
     return(
         <>
-            <HeaderCeo selected="user"/>
-            <UserCeo />
+            <HeaderCeo selected="shops"/>
+            <ShopsCeo />
         </>
     );
 }
---
+++ shop-system-ui/src/pages/ShopsCeo.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/shopsCeo.css";
+
+function ShopsCeo() {
+
+}
+
+export default ShopsCeo
