85c0ddddf0113e4f2e8bbfe23a032746f60e2ecc
grochochotowski <g.m.rochowski@gmail.com>
2024-02-15 09:57:09 +0100

    Make position another type and rename it

--- shop-system/shop-system/Entities/User.cs
+++ shop-system/shop-system/Entities/User.cs
@@ -8,8 +8,8 @@ namespace shop_system.Entities
         public string FirstName { get; set; }
         public string SecondName { get; set; }
         public string LastName { get; set; }
-        public string Position { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
+        public int PositionId { get; set; }
     }
 }
