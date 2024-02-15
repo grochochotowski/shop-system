e1862dbbdd5a648be24e97a7392037982175980f
grochochotowski <g.m.rochowski@gmail.com>
2024-02-15 10:15:45 +0100

    shop seeder fix

--- shop-system/shop-system/Shopseeder.cs
+++ shop-system/shop-system/Shopseeder.cs
@@ -11,6 +11,32 @@ namespace shop_system
             _context = context;
         }
 
+        public void Seed()
+        {
+            if (_context.Database.CanConnect())
+            {
+                if (!_context.Positions.Any())
+                {
+                    var positions = GetPositions();
+                    _context.Positions.AddRange(positions);
+                    _context.SaveChanges();
+                }
+            }
+        }
 
+        public IEnumerable<Position> GetPositions()
+        {
+            var postions = new List<Position>()
+            {
+                new Position { Id = 1, Name = "COO"},
+                new Position { Id = 2, Name = "Accountant"},
+                new Position { Id = 3, Name = "Manager"},
+                new Position { Id = 4, Name = "Deputy Manager"},
+                new Position { Id = 5, Name = "Decorator"},
+                new Position { Id = 6, Name = "Shop Assistant"}
+            };
+
+            return postions;
+        }
     }
 }
