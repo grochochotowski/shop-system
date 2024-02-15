35986f3a2b3e1dc425c4f480b1dae1355cd9b959
grochochotowski <g.m.rochowski@gmail.com>
2024-02-15 10:58:12 +0100

    Create account controller file

---
+++ shop-system/shop-system/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace shop_system.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        
+    }
+}
