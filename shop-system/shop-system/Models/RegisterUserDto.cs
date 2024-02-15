43e967b43fb44278bb815b2ed5a22428220b606c
grochochotowski <g.m.rochowski@gmail.com>
2024-02-15 11:13:28 +0100

    Create registrUserDto

--- shop-system/shop-system/Models/RegisterUserDto.cs
+++ shop-system/shop-system/Models/RegisterUserDto.cs
@@ -1,7 +1,28 @@
-﻿namespace shop_system.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace shop_system.Models
 {
     public class RegisterUserDto
     {
-
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string? SecondName { get; set; }
+        public string LastName { get; set; }
+        [Required]
+        public string Login { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+        [Required]
+        [MinLength(6)]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        public int? ShopId { get; set; }
+        [Required]
+        public int PositionId { get; set; }
     }
 }
