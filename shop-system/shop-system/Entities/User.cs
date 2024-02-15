1fd1a89bbed50bfb4c3b9ef3ee26034b5dfe3632
grochochotowski <g.m.rochowski@gmail.com>
2024-02-15 11:01:02 +0100

    Add login and password to user entity

--- shop-system/shop-system/Entities/User.cs
+++ shop-system/shop-system/Entities/User.cs
@@ -8,7 +8,9 @@ namespace shop_system.Entities
         public string FirstName { get; set; }
         public string SecondName { get; set; }
         public string LastName { get; set; }
+        public string Login { get; set; }
         public string Email { get; set; }
+        public string Password { get; set; }
         public string PhoneNumber { get; set; }
         public int? ShopId { get; set; }
         public int PositionId { get; set; }
