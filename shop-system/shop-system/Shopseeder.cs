fd29348c7feeddf659d3b5587810e911b67f4cc7
grochochotowski <g.m.rochowski@gmail.com>
2024-02-15 10:05:04 +0100

    Create shop seeder

---
+++ shop-system/shop-system/Shopseeder.cs
@@ -0,0 +1,14 @@
+﻿using shop_system.Entities;
+
+namespace shop_system
+{
+    public class Shopseeder
+    {
+        private readonly ShopDbContext _context;
+
+        public Shopseeder(ShopDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
