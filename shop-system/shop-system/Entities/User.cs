78213cb018ede38f81c7548b9ce827bf3cc91a76
grochochotowski <g.m.rochowski@gmail.com>
2024-02-15 09:41:52 +0100

    Create user entity

---
+++ shop-system/shop-system/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Identity.Client;
+
+namespace shop_system.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string secondName { get; set; }
+        public string lastName { get; set; }
+        public string position { get; set; }
+        public string email { get; set; }
+        public string phoneNumber { get; set; }
+    }
+}
