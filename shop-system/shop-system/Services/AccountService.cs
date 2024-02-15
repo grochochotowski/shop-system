4b6e9aa9c39b447d95a610e14fa28eb2ecd9e28d
grochochotowski <g.m.rochowski@gmail.com>
2024-02-15 11:21:16 +0100

    Create account service

--- shop-system/shop-system/Services/AccountService.cs
+++ shop-system/shop-system/Services/AccountService.cs
@@ -1,6 +1,17 @@
-﻿namespace shop_system.Services
+﻿using shop_system.Entities;
+using shop_system.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace shop_system.Services
 {
     public class AccountService
     {
+        private readonly ShopDbContext _context;
+
+        public AccountService(ShopDbContext context)
+        {
+            _context = context;
+        }
+        
     }
 }
