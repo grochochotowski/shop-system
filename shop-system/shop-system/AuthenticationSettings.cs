9b5643a077c36b9654a6e261fcd9d45844b23cee
grochochotowski <g.m.rochowski@gmail.com>
2024-02-16 11:10:03 +0100

    Create authentication settings

---
+++ shop-system/shop-system/AuthenticationSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace shop_system
+{
+    public class AuthenticationSettings
+    {
+        public string JwtKey { get; set; }
+        public int JwtExpiresDays { get; set; }
+        public string JwtIssuer { get; set; }
+    }
+}
