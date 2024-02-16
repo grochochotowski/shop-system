05952eb802ae9994fc9193c043dd9df31302ff51
grochochotowski <g.m.rochowski@gmail.com>
2024-02-16 15:56:16 +0100

    make email and login only login

--- shop-system/shop-system/Models/LoginDto.cs
+++ shop-system/shop-system/Models/LoginDto.cs
@@ -7,8 +7,6 @@ namespace shop_system.Models
         [Required]
         public string Login { get; set; }
         [Required]
-        public string Email { get; set; }
-        [Required]
         public string Password { get; set; }
     }
 }
