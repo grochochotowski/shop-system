36dbae03c5fdee2aaa2f32debb2161f184e29532
grochochotowski <g.m.rochowski@gmail.com>
2024-02-16 11:46:17 +0100

    Create lgoin dto model

---
+++ shop-system/shop-system/Models/LoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace shop_system.Models
+{
+    public class LoginDto
+    {
+        [Required]
+        public string Login { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
