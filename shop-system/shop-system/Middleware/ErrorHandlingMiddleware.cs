0e77ba660706ce56827f394d6686f977547bf1df
grochochotowski <g.m.rochowski@gmail.com>
2024-02-16 11:57:08 +0100

    Create bad request middlewate

---
+++ shop-system/shop-system/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using shop_system.Exceptions;
+
+namespace shop_system.Middleware
+{
+    public class ErrorHandlingMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next.Invoke(context);
+            }
+            catch (BadRequestException badRequestException)
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync(badRequestException.Message);
+            }
+        }
+    }
+}
