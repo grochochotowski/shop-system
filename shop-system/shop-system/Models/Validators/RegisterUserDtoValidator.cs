bbb348b70b4af76ccb685d624e97850faa7995bc
grochochotowski <g.m.rochowski@gmail.com>
2024-02-16 10:28:41 +0100

    Create validators for create user dto

--- shop-system/shop-system/Models/Validators/RegisterUserDtoValidator.cs
+++ shop-system/shop-system/Models/Validators/RegisterUserDtoValidator.cs
@@ -1,8 +1,32 @@
 ﻿using FluentValidation;
+using shop_system.Entities;
 namespace shop_system.Models.Validators
 {
     public class RegisterUserDtoValidator : AbstractValidator<RegisterUserDto>
     {
+        public RegisterUserDtoValidator(ShopDbContext dbContext)
+        {
+            // Email
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress()
+                .Custom((value, context) =>
+                {
+                    var emailInUse = dbContext.Users.Any(u => u.Email == value);
+                    if (emailInUse) context.AddFailure("Email", "Email is taken");
+                });
 
+            // Password
+            RuleFor(x => x.Password).MinimumLength(6);
+            RuleFor(x => x.ConfirmPassword).Equal(e => e.Password);
+
+            // Login
+            RuleFor(x => x.Login).NotEmpty()
+                .Custom((value, context) =>
+                {
+                    var loginInUse = dbContext.Users.Any(u => u.Login == value);
+                    if (loginInUse) context.AddFailure("Login", "Login is taken");
+                });
+        }
     }
 }
