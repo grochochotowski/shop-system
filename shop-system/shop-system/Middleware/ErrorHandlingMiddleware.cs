6b722e65b498d7b399994e5d239a2005f388901a
grochochotowski <g.m.rochowski@gmail.com>
2024-02-19 11:35:56 +0100

    Create catching general exceptions and log them

--- shop-system/shop-system/Middleware/ErrorHandlingMiddleware.cs
+++ shop-system/shop-system/Middleware/ErrorHandlingMiddleware.cs
@@ -6,16 +6,31 @@ namespace shop_system.Middleware
 {
     public class ErrorHandlingMiddleware : IMiddleware
     {
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+
+        public ErrorHandlingMiddleware(ILogger<ErrorHandlingMiddleware> logger)
+        {
+            _logger = logger;
+        }
+
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
             try
             {
                 await next.Invoke(context);
             }
+
             catch (BadRequestException badRequestException)
             {
                 context.Response.StatusCode = 400;
                 await context.Response.WriteAsync(badRequestException.Message);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsync("Something went wrong");
             }
         }
     }
