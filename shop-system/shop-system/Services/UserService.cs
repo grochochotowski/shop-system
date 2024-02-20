d82af307dda8330cf0cb4a08263eac188c629a36
grochochotowski <g.m.rochowski@gmail.com>
2024-02-20 13:38:27 +0100

    Copy other service getall to user service

--- shop-system/shop-system/Services/UserService.cs
+++ shop-system/shop-system/Services/UserService.cs
@@ -1,6 +1,42 @@
-﻿namespace shop_system.Services
+﻿using AutoMapper;
+using shop_system.Entities;
+using shop_system.Models;
+using shop_system.Serivces;
+
+namespace shop_system.Services
 {
     public class UserService
     {
+        public interface IShopService
+        {
+            IEnumerable<ShopDto> GetAll();
+        }
+
+        public class ShopService : IShopService, IShopService
+        {
+            private readonly ShopDbContext _context;
+            private readonly IMapper _mapper;
+
+            public ShopService(ShopDbContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+
+
+            public IEnumerable<ShopDto> GetAll()
+            {
+                var shops = _context
+                    .Shops
+                    .Include(s => s.Clothes)
+                        .ThenInclude(ca => ca.Clothing)
+                    .ToList();
+
+                var result = _mapper.Map<List<ShopDto>>(shops);
+
+                return result;
+            } // Get all users with their shops
+        }
     }
 }
