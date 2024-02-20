a8c2b1de4bc954ee8377658a886069a64957ead9
grochochotowski <g.m.rochowski@gmail.com>
2024-02-20 13:48:52 +0100

    Create query for get users with their shops

--- shop-system/shop-system/Services/UserService.cs
+++ shop-system/shop-system/Services/UserService.cs
@@ -12,7 +12,7 @@ namespace shop_system.Services
             IEnumerable<ShopDto> GetAll();
         }
 
-        public class ShopService : IShopService, IShopService
+        public class ShopService : IShopService
         {
             private readonly ShopDbContext _context;
             private readonly IMapper _mapper;
@@ -25,15 +25,25 @@ namespace shop_system.Services
 
 
 
-            public IEnumerable<ShopDto> GetAll()
+            public IEnumerable<GetUsersWithShopDto> GetAll()
             {
-                var shops = _context
-                    .Shops
-                    .Include(s => s.Clothes)
-                        .ThenInclude(ca => ca.Clothing)
-                    .ToList();
+                var shopsWithUsers = from s in _context.Shops
+                                     join u in _context.Users on s.Id equals u.ShopId into userGroup
+                                     from user in userGroup.DefaultIfEmpty()
+                                     orderby s.Id, user.PositionId
+                                     select new
+                                     {
+                                         Code = s.Code,
+                                         PositionId = user.PositionId,
+                                         Login = user.Login,
+                                         FirstName = user.FirstName,
+                                         SecondName = user.SecondName,
+                                         LastName = user.LastName,
+                                         Email = user.Email,
+                                         PhoneNumber = user.PhoneNumber
+                                     };
 
-                var result = _mapper.Map<List<ShopDto>>(shops);
+                var result = _mapper.Map<List<GetUsersWithShopDto>>(shopsWithUsers);
 
                 return result;
             } // Get all users with their shops
