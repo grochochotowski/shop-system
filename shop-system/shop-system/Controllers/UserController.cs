8a06af1380af10b7400f191775baf55ed2cfa123
grochochotowski <g.m.rochowski@gmail.com>
2024-02-20 13:34:32 +0100

    Copy other get and modify it to work with user

--- shop-system/shop-system/Controllers/UserController.cs
+++ shop-system/shop-system/Controllers/UserController.cs
@@ -1,6 +1,26 @@
-﻿namespace shop_system.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using shop_system.Models;
+using shop_system.Services;
+
+namespace shop_system.Controllers
 {
+    [Route("api/user")]
     public class UserController
     {
+        private readonly IUserSerivce _userService;
+
+        public UserController(IUserSerivce userService)
+        {
+            _userService = userService;
+        }
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ShopDto>> GetAll()
+        {
+            var usersWithShopDto = _userService.GetAll();
+
+            return Ok(usersWithShopDto);
+        } // Get all users with their shops
     }
 }
