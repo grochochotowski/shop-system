da6257d5f4dd6c358c9d28f333cb50ac80d2a760
grochochotowski <g.m.rochowski@gmail.com>
2024-02-21 11:10:31 +0100

    Fix classes

--- shop-system/shop-system/Services/UserService.cs
+++ shop-system/shop-system/Services/UserService.cs
@@ -1,52 +1,48 @@
 ﻿using AutoMapper;
 using shop_system.Entities;
 using shop_system.Models;
-using shop_system.Serivces;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace shop_system.Services
 {
-    public class UserService
+    public interface IUserService
     {
-        public interface IUserService
+        IEnumerable<GetUsersWithShopDto> GetAllUsersWithShop();
+    }
+
+    public class UserService : IUserService
+    {
+        private readonly ShopDbContext _context;
+        private readonly IMapper _mapper;
+
+        public UserService(ShopDbContext context, IMapper mapper)
         {
-            IEnumerable<GetUsersWithShopDto> GetAllUsersWithShop();
+            _context = context;
+            _mapper = mapper;
         }
 
-        public class ShopService : IUserService
+        public IEnumerable<GetUsersWithShopDto> GetAllUsersWithShop()
         {
-            private readonly ShopDbContext _context;
-            private readonly IMapper _mapper;
+            var shopsWithUsers = (from s in _context.Shops
+                                  join u in _context.Users on s.Id equals u.ShopId into userGroup
+                                  from user in userGroup.DefaultIfEmpty()
+                                  orderby s.Id, user.PositionId
+                                  select new
+                                  {
+                                      Code = s.Code,
+                                      PositionId = user.PositionId,
+                                      Login = user.Login,
+                                      FirstName = user.FirstName,
+                                      SecondName = user.SecondName,
+                                      LastName = user.LastName,
+                                      Email = user.Email,
+                                      PhoneNumber = user.PhoneNumber
+                                  }).ToList();
 
-            public ShopService(ShopDbContext context, IMapper mapper)
-            {
-                _context = context;
-                _mapper = mapper;
-            }
+            var result = _mapper.Map<List<GetUsersWithShopDto>>(shopsWithUsers);
 
-
-
-            public IEnumerable<GetUsersWithShopDto> GetAllUsersWithShop()
-            {
-                var shopsWithUsers = (from s in _context.Shops
-                                     join u in _context.Users on s.Id equals u.ShopId into userGroup
-                                     from user in userGroup.DefaultIfEmpty()
-                                     orderby s.Id, user.PositionId
-                                     select new
-                                     {
-                                         Code = s.Code,
-                                         PositionId = user.PositionId,
-                                         Login = user.Login,
-                                         FirstName = user.FirstName,
-                                         SecondName = user.SecondName,
-                                         LastName = user.LastName,
-                                         Email = user.Email,
-                                         PhoneNumber = user.PhoneNumber
-                                     }).ToList();
-
-                var result = _mapper.Map<List<GetUsersWithShopDto>>(shopsWithUsers);
-
-                return result;
-            } // Get all users with their shops
-        }
+            return result;
+        } // Get all users with their shops
     }
 }
