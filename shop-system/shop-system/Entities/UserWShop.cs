60f5470023956d531b88723a788aeca0eae68272
grochochotowski <g.m.rochowski@gmail.com>
2024-02-21 11:52:02 +0100

    Create user with shop entity

---
+++ shop-system/shop-system/Entities/UserWShop.cs
@@ -0,0 +1,14 @@
+﻿namespace shop_system.Entities
+{
+    public class UserWShop
+    {
+        public string? Code { get; set; }
+        public int PositionId { get; set; }
+        public string Login { get; set; }
+        public string FirstName { get; set; }
+        public string? SecondName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
