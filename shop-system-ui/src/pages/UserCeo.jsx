98abe1c31efeac8b4eb5245d400ce6ee3cca518d
grochochotowski <g.m.rochowski@gmail.com>
2024-02-22 13:09:01 +0100

    Check what getshopwithusers reurns

--- shop-system-ui/src/pages/UserCeo.jsx
+++ shop-system-ui/src/pages/UserCeo.jsx
@@ -64,6 +64,7 @@ function UserCeo() {
                         );
                     });
             }
+            console.log(usersInShop);
             return(
                 <ShopUserListElement
                     key={shopId}
