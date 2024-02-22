cf62af462cc9603679eb1c41f82e77cfc5180848
grochochotowski <g.m.rochowski@gmail.com>
2024-02-22 13:10:37 +0100

    Add id to userwshopdto

--- shop-system/shop-system/Models/UserWShopDto.cs
+++ shop-system/shop-system/Models/UserWShopDto.cs
@@ -5,6 +5,7 @@ namespace shop_system.Models
 {
     public class UserWShopDto
     {
+        public int Id { get; set; }
         public string Login { get; set; }
         public string FirstName { get; set; }
         public string? SecondName { get; set; }
