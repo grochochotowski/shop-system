c3921fea1080635c9a739a69f175ecc6d872aa84
grochochotowski <g.m.rochowski@gmail.com>
2024-02-22 10:39:20 +0100

    Rename model

---
+++ shop-system/shop-system/Migrations/20240220120116_addCodeValueToTheShopEntity.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace shop_system.Migrations
+{
+    /// <inheritdoc />
+    public partial class addCodeValueToTheShopEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "Shops",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_PositionId",
+                table: "Users",
+                column: "PositionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Positions_PositionId",
+                table: "Users",
+                column: "PositionId",
+                principalTable: "Positions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Positions_PositionId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_PositionId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "Shops");
+        }
+    }
+}
---
+++ shop-system/shop-system/Models/UsersWShopDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace shop_system.Models
+{
+    public class UsersWShopDto
+    {
+        public string? Code { get; set; }
+        public int PositionId { get; set; }
+        public string Login { get; set; }
+        public string FirstName { get; set; }
+        public string? SecondName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
