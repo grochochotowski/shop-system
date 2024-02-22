8e63628d8c3d522db43a209d72197e1651e9dba9
grochochotowski <g.m.rochowski@gmail.com>
2024-02-22 11:27:25 +0100

    Refctor

--- shop-system-ui/src/pages/UserCeo.jsx
+++ shop-system-ui/src/pages/UserCeo.jsx
@@ -12,20 +12,20 @@ function UserCeo() {
 
 
     // fetch api data - get info about users and thir shops
-    /*const fetchData = async () => {
-        const response = await fetch("https://localhost:7057/api/user");
-        const data = await response.json();
-        console.log(data);
-    }*/
     useEffect(() => {
+        fetch("https://localhost:7057/api/user")
+        .then(response => response.json())
+        .then(response => console.log(response));
+    }, []);
+
+    /*useEffect(() => {
         async function getUserData() {
             const response = await fetch("https://localhost:7057/api/user");
             const data = await response.json();
             console.log(data);
         }
-
         getUserData();
-    }, []);
+    }, []);*/
 
 
     // Calculate width of 2 main sections based on size-bar position
