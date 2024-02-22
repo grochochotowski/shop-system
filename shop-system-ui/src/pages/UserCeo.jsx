f9372717ae6f086c3f1832a47b67d8f53acb8ebf
grochochotowski <g.m.rochowski@gmail.com>
2024-02-22 11:21:04 +0100

    use useeffect to fetch data

--- shop-system-ui/src/pages/UserCeo.jsx
+++ shop-system-ui/src/pages/UserCeo.jsx
@@ -12,11 +12,16 @@ function UserCeo() {
 
 
     // fetch api data - get info about users and thir shops
-    const fetchData = async () => {
+    /*const fetchData = async () => {
         const response = await fetch("https://localhost:7057/api/user");
         const data = await response.json();
         console.log(data);
-    }
+    }*/
+    useEffect(() => {
+        fetch("https://localhost:7057/api/user")
+        .then(response => response.json())
+        .then(response => console.log(response));
+    }, []);
 
 
     // Calculate width of 2 main sections based on size-bar position
