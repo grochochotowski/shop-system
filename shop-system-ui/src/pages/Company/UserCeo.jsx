caad0d9860a3cf35e6e323b9dd9e62d51fdbe496
grochochotowski <g.m.rochowski@gmail.com>
2024-03-05 23:14:41 +0100

    Remove file not to get confused

--- shop-system-ui/src/pages/Company/UserCeo.jsx
+++ shop-system-ui/src/pages/Company/UserCeo.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react"
 import "../styles/userCeo.css"
-import UserFormCeo from "../components/UserFormCeo";
-import UserListCeo from "../components/UserListCeo";
+import UserFormCeo from "../../components/UserFormCeo";
+import UserListCeo from "../../components/UserListCeo";
 
 function UserCeo() {
     const [containerHeight, setContainerHeight] = useState("");
