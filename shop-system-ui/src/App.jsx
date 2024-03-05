c03ba185d86b193a82eb31865e0d86882d93c475
grochochotowski <g.m.rochowski@gmail.com>
2024-03-05 23:25:34 +0100

    Fix program structure errors

--- shop-system-ui/src/App.jsx
+++ shop-system-ui/src/App.jsx
@@ -3,8 +3,8 @@ import HeaderCeo from "./components/HeaderCeo";
 // PAGES
 import Welcome from "./pages/Welcome";
 import Login from "./pages/Login";
-import UserCeo from "../pages/company/UserCeo";
-import ShopsCeo from "../pages/company/ShopsCeo";
+import UserCeo from "./pages/UserCeo";
+import ShopsCeo from "./pages/ShopsCeo";
 
 
 function App() {
