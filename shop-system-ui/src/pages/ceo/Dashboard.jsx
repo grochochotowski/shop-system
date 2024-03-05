a8618472140db7838241dc636c661a0c31406b35
grochochotowski <g.m.rochowski@gmail.com>
2024-03-06 00:05:32 +0100

    Make temporary random images bigger

--- shop-system-ui/src/pages/ceo/Dashboard.jsx
+++ shop-system-ui/src/pages/ceo/Dashboard.jsx
@@ -5,12 +5,12 @@ import DashboardOption from "../../components/DashboardOption";
 function Dashboard() {
     return (
         <div className="dashboard-container">
-            <DashboardOption name="name 1" img="https://picsum.photos/200/300" />
-            <DashboardOption name="name 2" img="https://picsum.photos/200/300" />
-            <DashboardOption name="name 3" img="https://picsum.photos/200/300" />
-            <DashboardOption name="name 4" img="https://picsum.photos/200/300" />
-            <DashboardOption name="name 5" img="https://picsum.photos/200/300" />
-            <DashboardOption name="name 6" img="https://picsum.photos/200/300" />
+            <DashboardOption name="name 1" img="https://picsum.photos/1000/300" />
+            <DashboardOption name="name 2" img="https://picsum.photos/1000/300" />
+            <DashboardOption name="name 3" img="https://picsum.photos/1000/300" />
+            <DashboardOption name="name 4" img="https://picsum.photos/1000/300" />
+            <DashboardOption name="name 5" img="https://picsum.photos/1000/300" />
+            <DashboardOption name="name 6" img="https://picsum.photos/1000/300" />
         </div>
     );
 }
