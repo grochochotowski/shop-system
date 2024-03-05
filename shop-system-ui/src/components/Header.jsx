56c3310c19ed512a473c81ed7c9820c1fda79f1e
grochochotowski <g.m.rochowski@gmail.com>
2024-03-06 00:17:42 +0100

    Display logo in header

--- shop-system-ui/src/components/Header.jsx
+++ shop-system-ui/src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Logo from "../images/logo.png";
 
 function Header() {
     return (
         <header className="app-header">
-            <img className="app-logo" src="../../public/images/logo.png" alt="LOGO" />
+            <img className="app-logo" src={Logo} alt="LOGO" />
             <h3 className="app-language">Eng</h3>
         </header>
     );
