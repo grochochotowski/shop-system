4ab7ac412eb4e2e2d2b34e91624cdc6d88c61153
grochochotowski <g.m.rochowski@gmail.com>
2024-03-06 00:09:38 +0100

    Create new header

---
+++ shop-system-ui/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header className="App-header">
+            <img src="../../public/images/shopping-bag.png" alt="LOGO" />
+            <h3 className="App-language">Eng</h3>
+        </header>
+    );
+}
+
+export default Header;
