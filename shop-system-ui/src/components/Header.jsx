bcb3a0d73edd6fa52b8a123c73f9540a3b043f34
grochochotowski <g.m.rochowski@gmail.com>
2024-03-06 00:14:24 +0100

    Copy bag and name it logo

--- shop-system-ui/src/components/Header.jsx
+++ shop-system-ui/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
     return (
         <header className="app-header">
-            <img className="app-logo" src="../../public/images/shopping-bag.png" alt="LOGO" />
+            <img className="app-logo" src="../../public/images/logo.png" alt="LOGO" />
             <h3 className="app-language">Eng</h3>
         </header>
     );
