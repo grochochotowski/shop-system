caf38ad76fb0cd43340c5b22b986d49c8654adad
grochochotowski <g.m.rochowski@gmail.com>
2024-03-05 23:47:40 +0100

    Modify dashboard option component

--- shop-system-ui/src/components/DashboardOption.jsx
+++ shop-system-ui/src/components/DashboardOption.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-function DashboardOption () {
+function DashboardOption (props) {
     return (
-        <div>
-            <h1>Dashboard</h1>
+        <div className="option">
+            <h1>{props.name}</h1>
         </div>
     );
 }
