d66bc2d175f45a9c87a6eefec51414ecb77bb174
grochochotowski <g.m.rochowski@gmail.com>
2024-03-05 23:48:44 +0100

    Import and display dashboardoption components on dashboard page

--- shop-system-ui/src/pages/ceo/Dashboard.jsx
+++ shop-system-ui/src/pages/ceo/Dashboard.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "../../styles/ceo/dashboard.css";
+import DashboardOption from "../../components/DashboardOption";
 
 function Dashboard() {
     return (
         <div>
-            <h1>Dashboard</h1>
+            <DashboardOption name="name 1" />
+            <DashboardOption name="name 2" />
+            <DashboardOption name="name 3" />
+            <DashboardOption name="name 4" />
+            <DashboardOption name="name 5" />
         </div>
     );
 }
