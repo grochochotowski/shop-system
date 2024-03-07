5d97c416c93c327aa15244769d6bd031d2b7c800
grochochotowski <g.m.rochowski@gmail.com>
2024-03-07 19:15:02 +0100

    Upadte option with client image

--- shop-system-ui/src/pages/ceo/Dashboard.jsx
+++ shop-system-ui/src/pages/ceo/Dashboard.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import "../../styles/ceo/dashboard.css";
+/*Components*/
 import DashboardOption from "../../components/DashboardOption";
 import AppHeader from "../../components/AppHeader";
+/*Images*/
+import clientImg from "../../images/client.jpg"
 
 function Dashboard() {
     return (
         <>
             <AppHeader />
             <div className="dashboard-container">
-                <DashboardOption name="name 1" img="https://picsum.photos/1000/300" />
+                <DashboardOption name="name 1" img={clientImg} />
                 <DashboardOption name="name 2" img="https://picsum.photos/1000/300" />
                 <DashboardOption name="name 3" img="https://picsum.photos/1000/300" />
                 <DashboardOption name="name 4" img="https://picsum.photos/1000/300" />
