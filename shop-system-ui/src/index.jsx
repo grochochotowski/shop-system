10c2c844fd7a65d0455ffb3f02c62d1a94ac1e28
grochochotowski <g.m.rochowski@gmail.com>
2024-03-08 23:05:14 +0100

    Add BrowserRouter to index jsx file

--- shop-system-ui/src/index.jsx
+++ shop-system-ui/src/index.jsx
@@ -7,6 +7,7 @@ import global_eng from "./translations/eng/global.json";
 import global_pol from "./translations/pol/global.json";
 import i18next from "i18next";
 import {I18nextProvider} from "react-i18next"
+import {BrowserRouter} from "react-router-dom"
 
 i18next.init({
     interpolation: {escapeValue: false},
@@ -24,8 +25,10 @@ i18next.init({
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <I18nextProvider i18n={i18next}>
-            <App />
-        </I18nextProvider>
+        <BrowserRouter>
+            <I18nextProvider i18n={i18next}>
+                <App />
+            </I18nextProvider>
+        </BrowserRouter>
     </React.StrictMode>
 );
