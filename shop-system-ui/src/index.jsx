15c71ba0df78c4eec6be459a1693acbd3fb26be4
grochochotowski <g.m.rochowski@gmail.com>
2024-03-08 14:19:32 +0100

    import i18next provider

--- shop-system-ui/src/index.jsx
+++ shop-system-ui/src/index.jsx
@@ -5,7 +5,7 @@ import App from "./App"
 
 import global_eng from "./translations/eng/global.json";
 import global_pol from "./translations/pol/global.json";
-import i18next from "i18next";
+import i18next, {I18nextProvider} from "react-i18next";
 
 i18next.init({
     interpolation: {escapeValue: false},
@@ -23,6 +23,8 @@ i18next.init({
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+        <I18nextProvider i18n={i18next}>
+            <App />
+        </I18nextProvider>
     </React.StrictMode>
 );
