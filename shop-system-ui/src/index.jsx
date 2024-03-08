815a624f067644962853a56c08748b472da866ab
grochochotowski <g.m.rochowski@gmail.com>
2024-03-08 14:16:11 +0100

    Import i18next

--- shop-system-ui/src/index.jsx
+++ shop-system-ui/src/index.jsx
@@ -3,5 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App"
 
+import global_eng from "./translations/eng/global.json";
+import global_pol from "./translations/pol/global.json";
+import i18next from "i18next";
+
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
