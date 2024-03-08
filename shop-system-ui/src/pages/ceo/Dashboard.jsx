36fbce54ddca057808d6f61a2197a6a3cdedd2e9
grochochotowski <g.m.rochowski@gmail.com>
2024-03-08 12:28:29 +0100

    Add new options names and images

--- shop-system-ui/src/pages/ceo/Dashboard.jsx
+++ shop-system-ui/src/pages/ceo/Dashboard.jsx
@@ -5,6 +5,8 @@ import DashboardOption from "../../components/DashboardOption";
 import AppHeader from "../../components/AppHeader";
 /*Images*/
 import clientImg from "../../images/client.jpg"
+import clothingImg from "../../images/clothing.jpg"
+import companyImg from "../../images/company.jpg"
 
 function Dashboard() {
     return (
@@ -12,11 +14,11 @@ function Dashboard() {
             <AppHeader />
             <div className="dashboard-container">
                 <DashboardOption name="Clients" img={clientImg} />
-                <DashboardOption name="name 2" img="https://picsum.photos/1000/300" />
+                <DashboardOption name="Clothing" img={clothingImg}  />
                 <DashboardOption name="name 3" img="https://picsum.photos/1000/300" />
                 <DashboardOption name="name 4" img="https://picsum.photos/1000/300" />
                 <DashboardOption name="name 5" img="https://picsum.photos/1000/300" />
-                <DashboardOption name="name 6" img="https://picsum.photos/1000/300" />
+                <DashboardOption name="Company" img={companyImg} />
             </div>
         </>
     );
