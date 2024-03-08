614a1efc20ee6e23b60a1753b799897db133c4a8
grochochotowski <g.m.rochowski@gmail.com>
2024-03-08 23:11:01 +0100

    import all pages

--- shop-system-ui/src/App.jsx
+++ shop-system-ui/src/App.jsx
@@ -1,23 +1,31 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 // COMPONENTS
 import AppHeader from "./components/AppHeader";
 
 // PAGES
 import Dashboard from "./pages/ceo/Dashboard";
+import Clients from "./pages/ceo/Clients";
+import Stores from "./pages/ceo/Stores";
+import Clothing from "./pages/ceo/Clothing";
+import Employees from "./pages/ceo/Employees";
+import Sales from "./pages/ceo/Sales";
+import Company from "./pages/ceo/Company";
 
 
 function App() {
     return(
-        <>
-            <AppHeader />
-            <Dashboard />
-        </>
-    );
+    <>
+        <AppHeader />
+        <Routes>
+            
+        </Routes>
+    </>
+    )
 }
 
 export default App;
-
 
 /*
 ARCHIVE IMPORTS
