ce4e3767b9e292ef7c3ed60391c63a8b2c0aecda
grochochotowski <g.m.rochowski@gmail.com>
2024-03-09 10:05:43 +0100

    Create lazy components

--- shop-system-ui/src/App.jsx
+++ shop-system-ui/src/App.jsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, {Suspense, lazy} from "react";
 import { Routes, Route } from "react-router-dom";
 
 // COMPONENTS
 import AppHeader from "./components/AppHeader";
 
 // PAGES
-import Dashboard from "./pages/ceo/Dashboard";
+import Fallback from './Fallback';
+/*import Dashboard from "./pages/ceo/Dashboard";
 import Clients from "./pages/ceo/Clients";
 import Stores from "./pages/ceo/Stores";
 import Clothing from "./pages/ceo/Clothing";
 import Employees from "./pages/ceo/Employees";
 import Sales from "./pages/ceo/Sales";
-import Company from "./pages/ceo/Company";
+import Company from "./pages/ceo/Company";*/
+const Dashboard = lazy(() => import("./pages/ceo/Dashboard"))
+const Clients = lazy(() => import("./pages/ceo/Clients"))
+const Stores = lazy(() => import("./pages/ceo/Stores"))
+const Clothing = lazy(() => import("./pages/ceo/Clothing"))
+const Employees = lazy(() => import("./pages/ceo/Employees"))
+const Sales = lazy(() => import("./pages/ceo/Sales"))
+const Company = lazy(() => import("./pages/ceo/Company"))
 
 
 function App() {
@@ -19,7 +27,11 @@ function App() {
     <>
         <AppHeader />
         <Routes>
-            <Route path="/dashboard" element={<Dashboard />}/>
+            <Route path="/dashboard" element={
+                <Suspense fallback={<Fallback />}>
+                    <Dashboard />
+                </Suspense>
+            }/>
             <Route path="/clients" element={<Clients />}/>
             <Route path="/stores" element={<Stores />}/>
             <Route path="/clothing" element={<Clothing />}/>
