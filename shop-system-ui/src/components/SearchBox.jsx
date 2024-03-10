5729185bc8092d12c10bc479820dabceaf4aa7b9
grochochotowski <g.m.rochowski@gmail.com>
2024-03-10 22:42:43 +0100

    add input to search box component

--- shop-system-ui/src/components/SearchBox.jsx
+++ shop-system-ui/src/components/SearchBox.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-export default function SearchBox() {
+export default function SearchBox(props) {
     return (
-        <button className="search-button">
-            <i class="fa-solid fa-magnifying-glass"></i>
-        </button>
+        <div className="search-box">
+            <input className="search-input" type="text" id="client-search" name="client-search" placeholder="Search for client"/>
+            <button className="search-button">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </button>
+        </div>
     )
 }
