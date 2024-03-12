803bb088c516b45c428100ed47bb3f4cda2e1513
grochochotowski <g.m.rochowski@gmail.com>
2024-03-12 14:49:52 +0100

    try using filter options gewnerator

--- shop-system-ui/src/components/FilterBox.jsx
+++ shop-system-ui/src/components/FilterBox.jsx
@@ -18,19 +18,19 @@ export default function FilterBox({filters}) {
         console.log(opened);
     }
 
-    function generateFilterOptions() {
+    function GenerateFilterOptions() {
         opened.forEach(element => {
             return (
                 <li className={element ? "opened main" : "closed main"}>
                     <h4 onClick={() => toggleFilter(element)}>Option1</h4>
                     <ul className="inside">
-                        <generateFiltersInOption opt={element}/>
+                        <GenerateFiltersInOption opt={element}/>
                     </ul>
                 </li>
             )
         });
     }
-    function generateFiltersInOption({opt}) {
+    function GenerateFiltersInOption({opt}) {
         filters.opt.forEach(element => {
             return(
                 <li className="inside">
@@ -44,30 +44,7 @@ export default function FilterBox({filters}) {
     return (
         <div className="filter-box">
             <ul className="main">
-                <li className={opened.option1 ? "opened main" : "closed main"}>
-                    <h4 onClick={() => toggleFilter("option1")}>Option1</h4>
-                    <ul className="inside">
-                        <li className="inside"><input type="checkbox" name="opt-1-1" id="opt-1-1" />
-                            <label htmlFor="opt-1-1">opt-1-1</label>
-                        </li>
-                        <li className="inside"><input type="checkbox" name="opt-1-2" id="opt-1-2" />
-                            <label htmlFor="opt-1-2">opt-1-2</label>
-                        </li>
-                        <li className="inside"><input type="checkbox" name="opt-1-3" id="opt-1-3" />
-                            <label htmlFor="opt-1-3">opt-1-3</label>
-                        </li>
-                        <li className="inside"><input type="checkbox" name="opt-1-4" id="opt-1-4" />
-                            <label htmlFor="opt-1-4">opt-1-4</label>
-                        </li>
-                        <li className="inside"><input type="checkbox" name="opt-1-5" id="opt-1-5" />
-                            <label htmlFor="opt-1-5">opt-1-5</label>
-                        </li>
-                    </ul>
-                </li>
-                <li className={opened.option2 ? "opened main" : "closed main"} onClick={() => toggleFilter("option2")}>Option2</li>
-                <li className={opened.option3 ? "opened main" : "closed main"} onClick={() => toggleFilter("option3")}>Option3</li>
-                <li className={opened.option4 ? "opened main" : "closed main"} onClick={() => toggleFilter("option4")}>Option4</li>
-                <li className={opened.option5 ? "opened main" : "closed main"} onClick={() => toggleFilter("option5")}>Option5</li>
+                <GenerateFilterOptions />
             </ul>
             <button className="filter-button">Filter</button>
         </div>
