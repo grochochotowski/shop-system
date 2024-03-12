7d612ca75d7e79cf9addf33b62c73c49d354d78f
grochochotowski <g.m.rochowski@gmail.com>
2024-03-12 15:10:27 +0100

    change filters option to contain arrays

--- shop-system-ui/src/pages/ceo/Clients.jsx
+++ shop-system-ui/src/pages/ceo/Clients.jsx
@@ -7,116 +7,116 @@ import FilterBox from '../../components/FilterBox'
 function Clients() {
 
     const [filters, setFilters] = useState({
-        option1: {
-            opt_1_1: {
+        option1: [
+            {
                 name: "Opt-1-1",
                 checked: false
             },
-            opt_1_2: {
+            {
                 name: "Opt-1-2",
                 checked: false
             },
-            opt_1_3: {
+            {
                 name: "Opt-1-3",
                 checked: false
             },
-            opt_1_4: {
+            {
                 name: "Opt-1-4",
                 checked: false
             },
-            opt_1_5: {
+            {
                 name: "Opt-1-5",
                 checked: false
             }
-        },
-        option2: {
-            opt_2_1: {
+        ],
+        option2: [
+            {
                 name: "Opt-2-1",
                 checked: false
             },
-            opt_2_2: {
+            {
                 name: "Opt-2-2",
                 checked: false
             },
-            opt_2_3: {
+            {
                 name: "Opt-2-3",
                 checked: false
             },
-            opt_2_4: {
+            {
                 name: "Opt-2-4",
                 checked: false
             },
-            opt_2_5: {
+            {
                 name: "Opt-2-5",
                 checked: false
             }
-        },
-        option3: {
-            opt_3_1: {
+        ],
+        option3: [
+            {
                 name: "Opt-3-1",
                 checked: false
             },
-            opt_3_2: {
+            {
                 name: "Opt-3-2",
                 checked: false
             },
-            opt_3_3: {
+            {
                 name: "Opt-3-3",
                 checked: false
             },
-            opt_3_4: {
+            {
                 name: "Opt-3-4",
                 checked: false
             },
-            opt_3_5: {
+            {
                 name: "Opt-3-5",
                 checked: false
             }
-        },
-        option4: {
-            opt_4_1: {
+        ],
+        option4: [
+            {
                 name: "Opt-4-1",
                 checked: false
             },
-            opt_4_2: {
+            {
                 name: "Opt-4-2",
                 checked: false
             },
-            opt_4_3: {
+            {
                 name: "Opt-4-3",
                 checked: false
             },
-            opt_4_4: {
+            {
                 name: "Opt-4-4",
                 checked: false
             },
-            opt_4_5: {
+            {
                 name: "Opt-4-5",
                 checked: false
             }
-        },
-        option5: {
-            opt_5_1: {
+        ],
+        option5: [
+            {
                 name: "Opt-5-1",
                 checked: false
             },
-            opt_5_2: {
+            {
                 name: "Opt-5-2",
                 checked: false
             },
-            opt_5_3: {
+            {
                 name: "Opt-5-3",
                 checked: false
             },
-            opt_5_4: {
+            {
                 name: "Opt-5-4",
                 checked: false
             },
-            opt_5_5: {
+            {
                 name: "Opt-5-5",
                 checked: false
             }
-        }
+        ]
     });
 
     return (
