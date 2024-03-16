da922f0dadead993767c57b07eec5026677a3ae5
grochochotowski <g.m.rochowski@gmail.com>
2024-03-16 22:59:04 +0100

    add contsturcor for client service

--- shop-system/shop-system/Services/ClientService.cs
+++ shop-system/shop-system/Services/ClientService.cs
@@ -1,6 +1,17 @@
-﻿namespace shop_system.Services
+﻿using AutoMapper;
+using shop_system.Entities;
+
+namespace shop_system.Services
 {
     public class ClientService
     {
+        private readonly ShopDbContext _context;
+        private readonly IMapper _mapper;
+
+        public ClientService(ShopDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
     }
 }
