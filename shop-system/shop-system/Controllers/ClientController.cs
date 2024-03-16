f0a648f0b2b8148d047361ecbff05c2df2669a51
grochochotowski <g.m.rochowski@gmail.com>
2024-03-16 22:57:16 +0100

    add constructor to client controller

--- shop-system/shop-system/Controllers/ClientController.cs
+++ shop-system/shop-system/Controllers/ClientController.cs
@@ -1,8 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using shop_system.Services;
 
 namespace shop_system.Controllers
 {
+    [Route("api/clients")]
     public class ClientController : ControllerBase
     {
+        private readonly IClientService _clientService;
+
+        public ClothingController(IClientService clientService)
+        {
+            _clientService = clientService;
+        }
     }
 }
