5fdf1ea273bee39011dfc51bce6e78c4c17db667
grochochotowski <g.m.rochowski@gmail.com>
2024-03-16 22:25:51 +0100

    add props to address model

--- shop-system/shop-system/Models/Address/AddressDto.cs
+++ shop-system/shop-system/Models/Address/AddressDto.cs
@@ -1,6 +1,14 @@
-﻿namespace shop_system.Models.Address
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace shop_system.Models.Address
 {
     public class AddressDto
     {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Buidling { get; set; }
+        public string Premises { get; set; }
+        public string PostalCode { get; set; }
     }
 }
