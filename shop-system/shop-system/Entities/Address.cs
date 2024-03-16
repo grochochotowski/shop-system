716a4f7449000e44b10049bee87df3ed4f0828fc
grochochotowski <g.m.rochowski@gmail.com>
2024-03-16 11:33:30 +0100

    create address entity

---
+++ shop-system/shop-system/Entities/Address.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace shop_system.Entities
+{
+    public class Address
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Street { get; set; }
+        [Required]
+        public string Buidling { get; set; }
+        [Required]
+        public string Premises { get; set; }
+        [Required]
+        public string PostalCode { get; set; }
+    }
+}
