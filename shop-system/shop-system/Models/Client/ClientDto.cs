2efd42f18c302e8b1f780ea7755278705129e909
grochochotowski <g.m.rochowski@gmail.com>
2024-03-16 22:27:01 +0100

    add props to client model

--- shop-system/shop-system/Models/Client/ClientDto.cs
+++ shop-system/shop-system/Models/Client/ClientDto.cs
@@ -1,6 +1,11 @@
-﻿namespace shop_system.Models.Client
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace shop_system.Models.Client
 {
     public class ClientDto
     {
+        public string Name { get; set; }
+        public string? Notes { get; set; }
+        public int AddressId { get; set; }
     }
 }
