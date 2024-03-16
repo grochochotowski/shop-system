2edd51ac81654ff806395613240e326e9ec0b624
grochochotowski <g.m.rochowski@gmail.com>
2024-03-16 22:49:40 +0100

    update create cliuent model

--- shop-system/shop-system/Models/Address/AddressDto.cs
+++ shop-system/shop-system/Models/Address/AddressDto.cs
@@ -8,7 +8,7 @@ namespace shop_system.Models.Address
         public string City { get; set; }
         public string Street { get; set; }
         public string Buidling { get; set; }
-        public string Premises { get; set; }
+        public string? Premises { get; set; }
         public string PostalCode { get; set; }
     }
 }
--- shop-system/shop-system/Models/Client/CreateClientDto.cs
+++ shop-system/shop-system/Models/Client/CreateClientDto.cs
@@ -1,6 +1,26 @@
-﻿namespace shop_system.Models.Client
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace shop_system.Models.Client
 {
     public class CreateClientDto
     {
+        [Required]
+        public string Name { get; set; }
+        public string? Notes { get; set; }
+
+
+
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Street { get; set; }
+        [Required]
+        public string Buidling { get; set; }
+
+        public string? Premises { get; set; }
+        [Required]
+        public string PostalCode { get; set; }
     }
 }
