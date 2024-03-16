4ab8ad6172d8003564fb7ced34dabd0d6a31a0fe
grochochotowski <g.m.rochowski@gmail.com>
2024-03-16 11:33:22 +0100

    create client entity

---
+++ shop-system/shop-system/Entities/Client.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace shop_system.Entities
+{
+    public class Client
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int AddressID { get; set; }
+
+        public string? Notes { get; set; }
+
+        public virtual Address Address { get; set; }
+    }
+}
