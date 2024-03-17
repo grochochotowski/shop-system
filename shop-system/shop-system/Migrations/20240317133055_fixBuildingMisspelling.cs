eb63ff5bb25c6d0e2ded7e90315f28acc7a63ace
grochochotowski <g.m.rochowski@gmail.com>
2024-03-17 14:32:05 +0100

    migrate db

---
+++ shop-system/shop-system/Migrations/20240317133055_fixBuildingMisspelling.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace shop_system.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixBuildingMisspelling : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Buidling",
+                table: "Addresses",
+                newName: "Building");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Building",
+                table: "Addresses",
+                newName: "Buidling");
+        }
+    }
+}
