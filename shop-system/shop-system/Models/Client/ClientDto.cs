b5db519109dc169d65c8e380c44597c5f9283896
grochochotowski <g.m.rochowski@gmail.com>
2024-03-17 14:15:36 +0100

    update client model

--- shop-system/shop-system/Models/Client/ClientDto.cs
+++ shop-system/shop-system/Models/Client/ClientDto.cs
@@ -4,7 +4,10 @@ namespace shop_system.Models.Client
 {
     public class ClientDto
     {
+        public int Id { get; set; }
+        public string InvoiceType { get; set; }
         public string Name { get; set; }
+        public string? NIP { get; set; }
         public string? Notes { get; set; }
         public int AddressId { get; set; }
     }
