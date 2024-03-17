f052b285a0767f36ea7cb36b5b2bc190f4713b13
grochochotowski <g.m.rochowski@gmail.com>
2024-03-17 15:04:24 +0100

    creae model for creating user withou address

---
+++ shop-system/shop-system/Models/Client/CreateClientNoAddressDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace shop_system.Models.Client
+{
+    public class CreateClientNoAddressDto
+    {
+        public string InvoiceType { get; set; }
+        public string Name { get; set; }
+        public string? NIP { get; set; }
+        public string? Notes { get; set; }
+        public int AddressId { get; set; }
+    }
+}
