6ea2b1628cb4122739d2cac8a25ce5c292068143
grochochotowski <g.m.rochowski@gmail.com>
2024-03-17 14:18:47 +0100

    migrate database

--- shop-system/shop-system/Migrations/20240317131611_updatePropsOfClient.Designer.cs
+++ shop-system/shop-system/Migrations/20240317131611_updatePropsOfClient.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using shop_system.Entities;
 
@@ -11,9 +12,11 @@ using shop_system.Entities;
 namespace shop_system.Migrations
 {
     [DbContext(typeof(ShopDbContext))]
-    partial class ShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240317131611_updatePropsOfClient")]
+    partial class updatePropsOfClient
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ shop-system/shop-system/Migrations/20240317131611_updatePropsOfClient.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace shop_system.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatePropsOfClient : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "InvoiceType",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NIP",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InvoiceType",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "NIP",
+                table: "Clients");
+        }
+    }
+}
