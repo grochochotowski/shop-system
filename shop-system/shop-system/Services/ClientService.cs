bf85fed83a1d608aab461e237c98a1eac9f1fd5d
grochochotowski <g.m.rochowski@gmail.com>
2024-03-17 14:42:15 +0100

    add mehtod to find or create new address

--- shop-system/shop-system/Services/ClientService.cs
+++ shop-system/shop-system/Services/ClientService.cs
@@ -24,6 +24,27 @@ namespace shop_system.Services
         // Create new client
         public int CreateClient(CreateClientDto dto)
         {
+            var existingAddress = _context.Addresses.FirstOrDefault(a =>
+                a.Country == dto.Country &&
+                a.City == dto.City &&
+                a.Street == dto.Street &&
+                a.Building == dto.Building &&
+                a.Premises == dto.Premises &&
+                a.PostalCode == dto.PostalCode);
+
+            if (existingAddress == null)
+            {
+                var newAddress = _mapper.Map<Address>(dto);
+                _context.Addresses.Add(newAddress);
+                _context.SaveChanges();
+                dto.AddressId = newAddress.Id;
+            }
+            else
+            {
+                dto.AddressId = existingAddress.Id;
+            }
+
+
             var client = _mapper.Map<Client>(dto);
             _context.Clients.Add(client);
             _context.SaveChanges();
