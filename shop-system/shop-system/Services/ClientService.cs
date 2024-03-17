51548e7c447f08634f7d93253b533caaa4329d2a
grochochotowski <g.m.rochowski@gmail.com>
2024-03-17 14:33:46 +0100

    refactor

--- shop-system/shop-system/Services/ClientService.cs
+++ shop-system/shop-system/Services/ClientService.cs
@@ -24,15 +24,6 @@ namespace shop_system.Services
         // Create new client
         public int CreateClient(CreateClientDto dto)
         {
-            var address = _context.Addresses.FirstOrDefault(a =>
-            a.Country == dto.Country &&
-            a.City == dto.City &&
-            a.Street == dto.Street &&
-            a.Building == dto.Building &&
-            a.Premises == dto.Premises &&
-            a.PostalCode == dto.PostalCode);
-
-
             var client = _mapper.Map<Client>(dto);
             _context.Clients.Add(client);
             _context.SaveChanges();
