0fec59bdc9548592e57875d1aaac74015a27ceec
grochochotowski <g.m.rochowski@gmail.com>
2024-03-17 16:00:43 +0100

    check if client exists

--- shop-system/shop-system/Controllers/ClientController.cs
+++ shop-system/shop-system/Controllers/ClientController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using shop_system.Entities;
 using shop_system.Models.Client;
 using shop_system.Services;
 
@@ -9,19 +10,31 @@ namespace shop_system.Controllers
     public class ClientController : ControllerBase
     {
         private readonly IClientService _clientService;
+        private readonly ShopDbContext _context;
 
-        public ClientController(IClientService clientService)
+        public ClientController(IClientService clientService, ShopDbContext context)
         {
             _clientService = clientService;
+            _context = context;
         }
 
 
         [HttpPost("new")]
         public ActionResult CreateClient([FromBody] CreateClientDto dto)
         {
-            var newClientId = _clientService.CreateClient(dto);
+            Client? client = _context.Clients.FirstOrDefault(c => c.Name == dto.Name);
 
-            return Created($"api/clients/{newClientId}", null);
+            if (client == null)
+            {
+                var newClientId = _clientService.CreateClient(dto);
+                return Created($"api/clients/{newClientId}", null);
+            }
+            else
+            {
+                return Conflict($"Resource already exists under ID: api/clients/{client.Id}");
+            }
+
+            
         }
     }
 }
