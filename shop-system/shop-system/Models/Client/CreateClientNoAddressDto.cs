8634a6ae31cc6c8aa49f415eef15398d5bbcd116
grochochotowski <g.m.rochowski@gmail.com>
2024-03-17 15:16:16 +0100

    add constructor to CreateClientNoAddressDto

--- shop-system/shop-system/Models/Client/CreateClientNoAddressDto.cs
+++ shop-system/shop-system/Models/Client/CreateClientNoAddressDto.cs
@@ -9,5 +9,14 @@ namespace shop_system.Models.Client
         public string? NIP { get; set; }
         public string? Notes { get; set; }
         public int AddressId { get; set; }
+
+        public CreateClientNoAddressDto(string InvoiceType, string Name, string? NIP, string? Notes, int AddressId)
+        {
+            this.InvoiceType = InvoiceType;
+            this.Name = Name;
+            this.NIP = NIP;
+            this.Notes = Notes;
+            this.AddressId = AddressId;
+        }
     }
 }
