cf8bad823e4690b22224a3b3e03c78e2e63834db
grochochotowski <g.m.rochowski@gmail.com>
2024-03-24 22:17:22 +0100

    create return for stores

--- shop-system-ui/src/pages/ceo/Stores.jsx
+++ shop-system-ui/src/pages/ceo/Stores.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 function Stores() {
     return (
-        <div>Stores</div>
+        <div className="store-container">
+            <div className="store-manage">
+                <div className="left">
+                    <SearchBox name={t("stores.txt-search-for")}/>
+                    <h2 className="filter-heading">{t("stores.txt-filters")}:</h2>
+                    <FilterBox filters={filters} toggleCheck={changeChecked} names={names} stores={stores}/>
+                </div>
+                <div className="right">
+                    <button className="add-button">{t("stores.add-btn")}</button>    
+                </div>                
+            </div>
+            <div className="store-list">
+                <GenerateStoreTable />
+            </div>
+        </div>
     )
 }
 
