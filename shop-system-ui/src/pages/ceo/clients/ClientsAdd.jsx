736920d8b4cc4ec925eb52c7cb315762ca2a1e5b
grochochotowski <g.m.rochowski@gmail.com>
2024-03-26 13:59:04 +0100

    use translation on add client page

--- shop-system-ui/src/pages/ceo/clients/ClientsAdd.jsx
+++ shop-system-ui/src/pages/ceo/clients/ClientsAdd.jsx
@@ -36,31 +36,31 @@ export default function ClientsAdd() {
     return (
         <div className="add-client-container">
             <div className="user-part glassy">
-                <h2>Client details</h2>
-                <input name="client-name" type="text" placeholder="Client name"/>
+                <h2>{t("clients.add.user-part.header")}</h2>
+                <input name="client-name" type="text" placeholder={t("clients.add.user-part.name")}/>
                 <div className="invoice-container">
                     <select name="invoice-type" id="invoice-type-choice" onChange={() => changeSelect()}>
-                        <option value="Name">Name</option>
-                        <option value="Company">Company</option>
+                        <option value="Name">{t("clients.add.user-part.select.name-invoice")}</option>
+                        <option value="Company">{t("clients.add.user-part.select.company-invoice")}</option>
                     </select>
                     {addUserForm.invoiceType === "Company" ? <input name="nip" className="company-invoice-input" type="text" placeholder="NIP"/> : ""}
                 </div>
-                <textarea name="notes" id="notes" placeholder="Notes" maxLength="500"/>
+                <textarea name="notes" id="notes" placeholder={t("clients.add.user-part.notes")} maxLength="500"/>
             </div>
             <div className="address-part glassy">
-                <h2>Address details</h2>
-                <input name="country" type="text" placeholder="Country"/>
-                <input name="city" type="text" placeholder="City"/>
-                <input name="postal-code" type="text" placeholder="Postal code"/>
-                <input name="street" type="text" placeholder="Street"/>
+                <h2>{t("clients.add.address-part.header")}</h2>
+                <input name="country" type="text" placeholder={t("clients.add.address-part.country")}/>
+                <input name="city" type="text" placeholder={t("clients.add.address-part.city")}/>
+                <input name="postal-code" type="text" placeholder={t("clients.add.address-part.postal-code")}/>
+                <input name="street" type="text" placeholder={t("clients.add.address-part.street")}/>
                 <div className="building-premises-container">
-                    <input name="buidling" type="text" placeholder="Building"/>
+                    <input name="buidling" type="text" placeholder={t("clients.add.address-part.building")}/>
                     <p> / </p>
-                    <input name="premises" type="text" placeholder="Premises"/>
+                    <input name="premises" type="text" placeholder={t("clients.add.address-part.premises")}/>
                 </div>
             </div>
             <div className="submit-add-client-button">
-                <button className="add-button middle" onClick={() => validateForm()}>Add client</button>
+                <button className="add-button middle" onClick={() => validateForm()}>{t("clients.add.add-btn")}</button>
             </div>
         </div>
     )
