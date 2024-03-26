13d0798b2f56e682e6d81b4d514f2f19ee45d071
grochochotowski <g.m.rochowski@gmail.com>
2024-03-26 12:20:41 +0100

    create hiddeable header

--- shop-system-ui/src/components/AppHeader.jsx
+++ shop-system-ui/src/components/AppHeader.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import {useTranslation} from "react-i18next";
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 
 import "../styles/appHeader.css";
 import Logo from "../images/logo.svg";
+import { use } from "i18next";
 
 function Header() {  
 
+    const [headerPos, setHeaderPos] = useState(0); // 1-open, 0-close
     const [t, i18n] = useTranslation("global")
 
     function changelanguage() {
@@ -18,8 +20,15 @@ function Header() {
         console.log(i18n.language);
     }
 
+    function toggleHeaderPos() {
+        setHeaderPos(prev => {
+            if (prev === 1) return 0
+            return 1
+        })
+    }
+
     return (
-        <header className="app-header">
+        <header className={headerPos === 1 ? "app-header" : "app-header hidden"}>
             <Link className="app-logo" to="/dashboard">
                 <img className="app-logo" src={Logo} alt="LOGO" />
             </Link>
@@ -27,6 +36,9 @@ function Header() {
                 <div className={`app-language-over ${i18n.language}`}></div>
             </div>
             <button className="button app-sign-out">{t("header.button")}</button>
+            <div className="toggle" onClick={() => toggleHeaderPos()}>
+                {headerPos === 1 ? <i className="fa-solid fa-chevron-up toggle-icon"></i> : <i className="fa-solid fa-chevron-down toggle-icon"></i>}
+            </div>
         </header>
     );
 }
