bb6cf2dd8d955511e69cc0e1187b26d7fe233548
grochochotowski <g.m.rochowski@gmail.com>
2024-03-28 09:43:57 +0100

    add dit-cutton class for edit button

--- shop-system-ui/src/pages/ceo/clients/ClientsDetails.jsx
+++ shop-system-ui/src/pages/ceo/clients/ClientsDetails.jsx
@@ -28,7 +28,7 @@ export default function ClientsDetails() {
     return (
         <div className="client-details-container">
             <div className="manage-client">
-                <Link to={`/clients/edit/${client.id}`}>Edit client</Link>
+                <Link to={`/clients/edit/${client.id}`} className="edit-button">Edit client</Link>
                 <button className="remove">Remove client</button>
             </div>
             <div className="display-client">
