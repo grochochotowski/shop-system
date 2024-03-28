ea3506dc80eed4419a30d9234c2ea994125322b6
grochochotowski <g.m.rochowski@gmail.com>
2024-03-28 09:13:44 +0100

    fetch details from api

--- shop-system-ui/src/pages/ceo/clients/ClientsDetails.jsx
+++ shop-system-ui/src/pages/ceo/clients/ClientsDetails.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from "react-router-dom";
 
 export default function ClientsDetails() {
 
   const { id } = useParams();
+  const [client, setClient] = useState({});
+
+
+  useEffect(() => {
+    async function getUserData() {
+        try {
+            const response = await fetch(`https://localhost:7057/api/clients/details/client-${id}`);
+            if (!response.ok) {
+                throw new Error('Failed to fetch data - ', response.status);
+            }
+            const data = await response.json();
+            setClient(data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+    getUserData();
+}, []);
 
   return (
     <div>ClientsDetails {id}</div>
