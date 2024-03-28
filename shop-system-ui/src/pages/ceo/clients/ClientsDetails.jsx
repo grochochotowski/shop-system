aebd84ffc3ffbb1d9f5e1b4aa05adac4a5c00c61
grochochotowski <g.m.rochowski@gmail.com>
2024-03-28 09:28:32 +0100

    create base layout for details about client

--- shop-system-ui/src/pages/ceo/clients/ClientsDetails.jsx
+++ shop-system-ui/src/pages/ceo/clients/ClientsDetails.jsx
@@ -1,29 +1,58 @@
-import React, { useState, useEffect } from 'react'
-import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
 
 export default function ClientsDetails() {
+    const { id } = useParams();
+    const [client, setClient] = useState({});
 
-  const { id } = useParams();
-  const [client, setClient] = useState({});
-
-
-  useEffect(() => {
-    async function getUserData() {
+    useEffect(() => {
+        async function getUserData() {
         try {
-            const response = await fetch(`https://localhost:7057/api/clients/details/client-${id}`);
+            const response = await fetch(
+            `https://localhost:7057/api/clients/details/client-${id}`
+            );
             if (!response.ok) {
-                throw new Error('Failed to fetch data - ', response.status);
+            throw new Error("Failed to fetch data - ", response.status);
             }
             const data = await response.json();
             setClient(data);
         } catch (error) {
-            console.error('Error fetching data:', error);
+            console.error("Error fetching data:", error);
         }
-    }
-    getUserData();
-}, []);
+        }
+        getUserData();
+    }, []);
 
-  return (
-    <div>ClientsDetails {id}</div>
-  )
+    return (
+        <div className="details-container">
+        <div className="manage-client">
+            <Link to={`/clients/edit/${client.id}`}>Edit client</Link>
+            <button className="remove">Remove client</button>
+        </div>
+            <div className="display-client">
+                <div className="client-box glassy">
+                    <h1>Client ID and name</h1>
+                    <div className="invoice-info">
+                        <p>invoiceType</p>
+                        <p>nip</p>
+                    </div>
+                </div>
+                <div className="address-box glassy">
+                    <div className="layer">
+                        <p>country</p>
+                        <p>city</p>
+                    </div>
+                    <div className="layer">
+                        <p>postalCode</p>
+                        <p>street</p>
+                    </div>
+                    <div className="layer">
+                        <p>buidling</p>
+                        <p>/</p>
+                        <p>premises</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
