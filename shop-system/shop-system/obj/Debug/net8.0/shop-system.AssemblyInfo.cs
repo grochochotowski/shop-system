9540ef4c930e6137baae4c80949c7b4c1b42dc5a
grochochotowski <g.m.rochowski@gmail.com>
2024-03-28 09:16:13 +0100

    add gitignore

--- shop-system/shop-system/obj/Debug/net8.0/shop-system.AssemblyInfo.cs
+++ shop-system/shop-system/obj/Debug/net8.0/shop-system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("shop-system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03b107e1a7f27359778e247da6ad7c8a56809510")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18198b6a216c69d94d555c3148f5f8649e9ba904")]
 [assembly: System.Reflection.AssemblyProductAttribute("shop-system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("shop-system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
